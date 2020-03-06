57e43953c0fdea22c0c455bfb87e4864886966ad
librewish <librewish@gmail.com>
2020-03-06 22:16:27 +0530

    status checking

--- scripts/check-audio-mute.sh
+++ scripts/check-audio-mute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ pacmd list-sinks | grep -A11 $(pacmd stat | awk -F": " '/^Default sink name: /{print $2}') | awk '/muted/ { print $NF }' == "yes" ]; then
+if [ $(pacmd list-sinks | grep -A11 $(pacmd stat | awk -F": " '/^Default sink name: /{print $2}') | awk '/muted/ { print $NF }') == "yes" ]; then
 echo "muted"
-elif [ pacmd list-sinks | grep -A11 $(pacmd stat | awk -F": " '/^Default sink name: /{print $2}') | awk '/muted/ { print $NF }' == "no" ]; then
+elif [ $(pacmd list-sinks | grep -A11 $(pacmd stat | awk -F": " '/^Default sink name: /{print $2}') | awk '/muted/ { print $NF }') == "no" ]; then
 echo "unmuted"
 fi
--- scripts/check-mic-mute.sh
+++ scripts/check-mic-mute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ pacmd list-sources | grep -A11 $(pacmd stat | awk -F": " '/^Default source name: /{print $2}') | awk '/muted/ { print $NF }' == "yes" ]; then
+if [ $(pacmd list-sources | grep -A11 $(pacmd stat | awk -F": " '/^Default source name: /{print $2}') | awk '/muted/ { print $NF }') == "yes" ]; then
 echo "muted"
-elif [ pacmd list-sources | grep -A11 $(pacmd stat | awk -F": " '/^Default source name: /{print $2}') | awk '/muted/ { print $NF }' == "no" ]; then
+elif [ $(pacmd list-sources | grep -A11 $(pacmd stat | awk -F": " '/^Default source name: /{print $2}') | awk '/muted/ { print $NF }') == "no" ]; then
 echo "unmuted"
 fi
