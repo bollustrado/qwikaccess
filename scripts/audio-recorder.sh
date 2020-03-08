ab40068619c3af7a0113b21459fedf7e1888b70a
librewish <librewish@gmail.com>
2020-03-08 15:40:18 +0530

    make changes so pkexec works in wayland

--- scripts/audio-recorder.sh
+++ scripts/audio-recorder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ffmpeg -f pulse -ac 2 -i default ~/Music/qwikaudiorecorder-$(date "+%Y-%m-%d-%H-%M-%S").mp3 ;
-notify-send -i "microphone-sensitivity-high" 'qwikaudiorecorder' 'recording started. will be saved in ~/Music'
+notify-send -i "microphone-sensitivity-high" 'qwikaudiorecorder' 'recording saved in ~/Music'
