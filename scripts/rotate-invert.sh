a1d94624ac0523a701cdad8892c232c115962cd6
librewish <librewish@gmail.com>
2020-03-09 20:57:27 +0530

    rotate using kscreendoctor

--- scripts/rotate-invert.sh
+++ scripts/rotate-invert.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+kscreen-doctor output."$(kscreen-doctor -o | grep "primary" | awk '/Output/ { print $3 }')".rotation.invert
 xrandr --output $(xrandr | egrep -o '^.+ connected' | cut -d " " -f 1) --rotate invert
